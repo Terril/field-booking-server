c9f14b7704a91ca74c027e4aba8b52d1afb40678
Terril Thomas <vente9@sg.uob>
2020-03-05 15:58:14 +0800

    Update data model for fields

--- src/main/kotlin/com/fielldBooking/apiRequest/controller/SportsListingController.kt
+++ src/main/kotlin/com/fielldBooking/apiRequest/controller/SportsListingController.kt
@@ -6,12 +6,12 @@ import com.fielldBooking.apiRequest.datamodel.SportsList
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.concurrent.atomic.AtomicLong
+import java.util.concurrent.atomic.AtomicInteger
 
 @RestController
 @RequestMapping("/sports")
 class SportsListingController {
-    val counter = AtomicLong()
+    val counter = AtomicInteger()
 
     @GetMapping("/list")
     fun sportsListing(): List<SportsList> {
--- src/main/kotlin/com/fielldBooking/apiRequest/datamodel/Listing.kt
+++ src/main/kotlin/com/fielldBooking/apiRequest/datamodel/Listing.kt
@@ -1,17 +1,20 @@
 package com.fielldBooking.apiRequest.datamodel
 
-data class SportsList(val id: Long, val name: String, val imageUrl: String)
+data class SportsList(val id: Int, val name: String, val imageUrl: String)
 
 data class FieldsList(val fields: List<Fields>)
 
-data class Fields(val id: Long,
+data class Fields(val id: Int,
                   val title: String,
                   val fieldType: String,
                   val address: String,
                   val lat: Double,
                   val lon: Double,
-                  val phoneNumber: Long,
+                  val phoneNumber: Int,
                   val contactPerson: String,
-                  val whatsappNumber: Long,
+                  val whatsappNumber: Int,
                   val emailAddress: String,
-                  val likes: Long)
+                  val likes: Int,
+                  var visitedFriends: List<VisitedFriend> = emptyList())
+
+data class VisitedFriend(val id: Int, val name: String, val phoneNumber: Int, val visitedTimes: Int)
