1f590501d165a02e2c8fb617e97e047b14a7b22a
Terril Thomas <vente9@sg.uob>
2020-03-06 17:11:15 +0800

    String extension and Utils for mongoDB

---
+++ src/main/kotlin/com/fielldBooking/apiRequest/mongodb/util/Objects.kt
@@ -0,0 +1,7 @@
+package com.fielldBooking.apiRequest.mongodb.util
+
+import java.time.format.DateTimeFormatter
+
+object Objects {
+    val dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+}
